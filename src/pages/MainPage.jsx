3ffb74d98e6e763af521eac137b65d07cb20438c
Leeseul-Moon <leeseulmoon4@gmail.com>
2022-09-05 10:14:01 +0900

    remove package.lock

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,12 +3,12 @@ import styled from "styled-components";
 import HeaderContainer from "../components/Header/HeaderContainer";
 
 const MainPage = (props) => (
-  <AppContainer>
+  <MainContainer>
     <HeaderContainer />
-  </AppContainer>
+  </MainContainer>
 );
 
-const AppContainer = styled.div`
+const MainContainer = styled.div`
   height: 100vh;
 `;
 
